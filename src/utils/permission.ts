fd670729bd0d1750bceac7bb86a0eff89bfd4b63
Aak <anschluss_zeit@outlook.com>
2026-02-12 15:28:06 +0800

    refactor: improve formatting of group member info retrieval for consistency

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -8,11 +8,11 @@ export function isSuperUser(id: number) {
 
 export async function isAdminByData(client: NapLink, data: OneBotV11.GroupMessageEvent): Promise<boolean> {
     const { group_id, user_id: id } = data;
-    const memberInfo = await client.getGroupMemberInfo(group_id, id) as OneBotV11.GroupMemberInfo;
+    const memberInfo = (await client.getGroupMemberInfo(group_id, id)) as OneBotV11.GroupMemberInfo;
     return memberInfo.role === 'admin' || memberInfo.role === 'owner';
 }
 
 export async function isAdminById(client: NapLink, userId: number, groupId: number): Promise<boolean> {
-    const memberInfo = await client.getGroupMemberInfo(groupId, userId) as OneBotV11.GroupMemberInfo;
+    const memberInfo = (await client.getGroupMemberInfo(groupId, userId)) as OneBotV11.GroupMemberInfo;
     return memberInfo.role === 'admin' || memberInfo.role === 'owner';
 }
