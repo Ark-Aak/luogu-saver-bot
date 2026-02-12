a99bc11e86baa7faa311350f56c2496fa21d62ab
Aak <anschluss_zeit@outlook.com>
2026-02-12 23:34:29 +0800

    feat: extend Date prototype to customize locale string format

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+Date.prototype.toLocaleString = function() {
+    const Y = this.getFullYear();
+    const M = String(this.getMonth() + 1).padStart(2, '0');
+    const D = String(this.getDate()).padStart(2, '0');
+    const h = String(this.getHours()).padStart(2, '0');
+    const m = String(this.getMinutes()).padStart(2, '0');
+    const s = String(this.getSeconds()).padStart(2, '0');
+    return `${Y}/${M}/${D} ${h}:${m}:${s}`;
+};
+
 import { client } from '@/napcat-client';
 
 import { setupMessageHandler } from '@/handlers/message.handler';
